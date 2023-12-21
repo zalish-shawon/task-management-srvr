cff5636feb5ce328e5539deb91ae3ee3a7425e30
zalish-shawon <zalish.shawon@gmail.com>
2023-12-21 22:45:41 +0600

    api added

---
+++ index.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+require('dotenv').config()
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const port= process.env.PORT || 5000
+
+
+app.use(cors());
+app.use(express.json());
+
+
+const uri = "mongodb+srv://task-management:34pUbDM1u3AXKSYH@cluster0.hrjn1tt.mongodb.net/?retryWrites=true&w=majority";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    const taskCollection = client.db('taskDB').collection('tasks');
+
+    app.get('/tasks' , async(req, res) => {
+      const cursor = taskCollection.find();
+      const result = await cursor.toArray();
+      res.send(result);
+  });
+
+  app.post("/tasks", async(req, res) => {
+    const task = req.body;
+     console.log(task);
+    // const result = await taskCollection.insertOne(task);
+    // res.send(result);
+});
+
+
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+app.get('/', (req, res) => {
+    res.send('Task management server is running')
+})
+
+app.listen(port, (req, res) => {
+    console.log(`listening on ${port}`);
+})
