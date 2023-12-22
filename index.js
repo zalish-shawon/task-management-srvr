d873243b6a4c8548e5bfe964b18643e0eb55aa31
zalish-shawon <zalish.shawon@gmail.com>
2023-12-22 16:44:21 +0600

    updaetd

--- index.js
+++ index.js
@@ -32,6 +32,20 @@ async function run() {
       res.send(result);
   });
 
+  app.get("/tasks/:id", async(req, res) => {
+    const id = req.params.id;
+    const query = {_id: new ObjectId(id)};
+    const result = await taskCollection.findOne(query);
+    res.send(result);
+  })
+
+  app.delete("/tasks/:id", async(req, res) => {
+    const id = req.params.id
+    const query = {_id: new ObjectId(id)}
+    const result = await taskCollection.deleteOne(query)
+    res.send(result);
+  });
+
   app.post("/tasks", async(req, res) => {
     const task = req.body;
      console.log(task);
